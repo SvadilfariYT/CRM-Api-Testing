8596cb7a2a0e7de87fe44c794f60326555c9b558
SvadilfariYT <dominik.sekular@gmail.com>
2024-05-18 20:07:55 +0200

    logging into text file

--- server.js
+++ server.js
@@ -1,5 +1,16 @@
 const express = require('express');
+const fs = require('fs');
 const app = express();
+
+// Create a write stream in append mode
+const accessLogStream = fs.createWriteStream('./access.log', { flags: 'a' });
+
+// Middleware to log each request to a file
+app.use((req, res, next) => {
+    const log = `Received ${req.method} request for '${req.url}' at ${new Date().toISOString()}\n`;
+    accessLogStream.write(log);
+    next();
+});
 
 // Define a route for the root URL
 app.get('/', (req, res) => {
