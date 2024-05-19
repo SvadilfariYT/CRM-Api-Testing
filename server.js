fe7411470b20a52a2c7c8e107da1bba7b62bb687
SvadilfariYT <dominik.sekular@gmail.com>
2024-05-19 13:11:03 +0200

    fixed an issue with the logging

--- server.js
+++ server.js
@@ -40,7 +40,7 @@ app.post('/api/data', (req, res) => {
 
 app.post('/webhook', (req, res) => {
     const { name, age } = req.body;  // Access the name and age sent by the client
-    accessLogStreamconsole.log(`Webhook received: ${name} (${age})`);
+    accessLogStream.write(`Webhook received: ${name} (${age})`);
 
     // Respond to the source system that the webhook was received and processed
     res.json({ status: 'success', message: 'Webhook processed' });
